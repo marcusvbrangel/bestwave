dcea2b68db81a1853d7d40dc2d225bbfe73f91af
Marcus Rangel <marcus.vbrangel@gmail.com>
2022-03-18 16:06:51 -0300

    feat: add first functional test

---
+++ test/jest-setup.ts
@@ -0,0 +1,10 @@
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
+
+let server: SetupServer;
+
+beforeAll(async () => {
+  server = new SetupServer();
+  await server.init();
+  global.testRequest = supertest(server.getApp());
+});
