dc5b720dee147a9cf85d35bfec315dde16010a0b
Marcus Rangel <marcus.vbrangel@gmail.com>
2022-03-28 13:44:15 -0300

    ref: change the type of mock axios

--- src/clients/__tests__/stormGlass.test.ts
+++ src/clients/__tests__/stormGlass.test.ts
@@ -9,12 +9,14 @@ jest.mock('axios');
 
 describe('StormGlass client', () => {
 
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it('should return the normalized forecast from the StormGlass service', async () => {
 
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
